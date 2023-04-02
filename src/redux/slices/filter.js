1934d93bea064cf7f010dba9e340c21c2492bb08
Lirika12 <uskova-tatyana@mail.ru>
2023-04-02 22:24:17 +0700

    change2

--- src/redux/slices/filter.js
+++ src/redux/slices/filter.js
@@ -1,24 +1,16 @@
-import { createSlice } from '@reduxjs/toolkit'
-import { initialFilterState } from '../initialValues'
-// import { initialUserState } from '../initialValues'
-
+import { createSlice } from "@reduxjs/toolkit";
+import { initialFilterState } from "../initialValues";
 
 export const filterSlice = createSlice({
-    name: 'filter',
-    initialState: {
-      value: initialFilterState
+  name: "filter",
+  initialState: initialFilterState,
+  reducers: {
+    changeSearchValue: (state, action) => {
+      state.search = action.payload;
     },
-    reducers: {
-      changeSearchValue: (state, action) => {
-        state.search = action.payload
-      },
-      removeUser: () => {
-        localStorage.clear()
-        return initialFilterState
-      }
-    }
-  })
+  },
+});
 
-  export const { changeSearchValue } = filterSlice.actions
+export const { changeSearchValue } = filterSlice.actions;
 
-  export const filterReducer = filterSlice.reducer
+export const filterReducer = filterSlice.reducer;
